b98f5a22ff3ae01a15867029cbcfbd51780c0554
Yejin <yejin-kim@lineplus.com>
2015-03-04 22:10:12 +0900

    인증키, 스프링 설정 추가

--- src/com/yejin/main/CrawlerMain.java
+++ src/com/yejin/main/CrawlerMain.java
@@ -2,12 +2,10 @@ package com.yejin.main;
 
 
 public class CrawlerMain {
-
+	// 1ef3bccb-79b1-33ca-b42a-52e7c1e1eb65
 	public static void main(String[] args) {
 		// 가수 검색으로 아이디 받아오기
 		// 챠트 파인더
 	}
-
-	
 
 }
