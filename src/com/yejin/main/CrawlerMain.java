84565a6559b9a79f03e9e4ad0261c2bd12ce7e09
Yejin <yejin-kim@lineplus.com>
2015-03-05 20:54:10 +0900

    테스트

--- src/com/yejin/main/CrawlerMain.java
+++ src/com/yejin/main/CrawlerMain.java
@@ -6,6 +6,14 @@ public class CrawlerMain {
 	public static void main(String[] args) {
 		// 가수 검색으로 아이디 받아오기
 		// 챠트 파인더
+		
+		int pageNum = 1;
+		String keyword = "나얼";
+		String appKey="d0199ef9-5405-3b6a-9f84-ae6b8007c368";
+		String uri = "http://apis.skplanetx.com/melon/songs?appKey="+appKey+"&format=json&count=50&page="+
+						pageNum+"&searchKeyword="+keyword+"&version=1";
+		
+		System.out.println(uri);
 	}
 
 }
